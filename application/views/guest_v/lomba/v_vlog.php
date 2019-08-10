1ef964222ce5505219cb37a7e802ff51ae4cae80
gayeng <52235336+gayeng@users.noreply.github.com>
2019-08-11 00:14:51 +0700

    Materi lomba gubernur
    baru

---
+++ application/views/guest_v/lomba/v_vlog.php
@@ -0,0 +1,97 @@
+<body>
+
+<section class="banner-area relative section-gap" id="home">  
+<div class="overlay overlay-bg"></div>
+        <div class="container">
+          <div class="row fullscreen d-flex align-items-center justify-content-center">
+            <div class="banner-content col-lg-9 col-md-12" >
+              
+              <h1 class="text-white">
+                Lomba-Lomba Piala Gubernur
+              </h1>
+              <br>
+            </div>                      
+          </div>
+        </div>
+</section>
+
+<div class="container">
+
+   <h1 class="mt-4 mb-3">
+      <small></small>
+    </h1>
+ <div class="container"> 
+ <section class="home-aboutus-area py-4">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+		
+			 <h2> <font color="white">Lomba Piala Gubenur Yang Di Perlombakan</font></h2>
+			</h1>
+			</div>
+			</br>
+			</section>
+			
+    <div>
+	<br>
+	</br>
+	</div>
+     <div class="row justify-content-md-center" >
+         <div class="col-lg-1"><a class="button" href="<?php echo base_url('Lomba/Gubernur'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/robot.png" alt="Lights" class="img img-fluid">
+		
+		</a></div>
+
+          <div class="col-lg-1"><a class="button" href="<?php echo base_url('Vlog'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/vlog.png" alt="Lights" class="img img-fluid">
+		
+		</a></div>
+        <div class="col-lg-1"><a class="button" href="<?php echo base_url('Gameo'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/gameonline.png" alt="Lights" class="img img-fluid">
+	    </p>
+		</a></div>
+         <div class="col-lg-1"><a class="button" href="<?php echo base_url('Fashionku'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/fashion.png" alt="Lights" class="img img-fluid">
+		</a></div>
+         <div class="col-lg-1"><a class="button" href="<?php echo base_url('Disabillitas'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/dissabilitas.png" alt="Lights" class="img img-fluid">
+		</a></div>
+         <div class="col-lg-1"><a class="button" href="<?php echo base_url('Fotocom'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/foto.png" alt="Lights" class="img img-fluid">
+		</a></div>
+		<div class="col-lg-1"><a class="button" href="<?php echo base_url('Band'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/band.png" alt="Lights" class="img img-fluid">
+		</a></div>
+		<div class="col-lg-1"><a class="button" href="<?php echo base_url('Lukis'); ?>"><img src="<?php echo base_url(); ?>template/img/icon/lukis.png" alt="Lights" class="img img-fluid">
+		</a></div>
+        
+      </div>
+	   </div>
+	  <br>
+	 <section class="home-aboutus-area py-4">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+			</div>
+			</br>
+			</section>
+			
+   <!-- Blog Post -->
+    <div class="card mb-4">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-5">
+            <a href="#">
+			   <img class="img img-fluid" height="100%" src="<?php echo base_url('template/img/lomba_gubernur/vlog.jpg') ?>" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+		  <br>
+            <h3 class="card-title">LOMBA VLOG</h3>
+			
+            <p class="card-text">Perkembangan Vlog sampai sekarang memang mengalami perkembangan yang cukup signifikan. Sejak kemunculan YouTube,perkembangan Vlogger ini memang cukup popular di banyak negara. Yuk Sobat , tunjukkan semangatmuu dengan mengikuti lomba Vlog ini
+			.Tunjukan semangat kreatif sobat disini memlalui vlog dengan tema "TEMATIK IPTEK dan Inovasi dalam industri Kreatif 4.0".
+			</p>
+     <h5 class="card-title"><a i class="fa fa-download" style="font-size:20px" href="<?php echo base_url().'Vlog/lakukan_download' ?>">Download Persyaratan Lomba Disini</i></a></h4>
+          </div>
+        </div>
+      </div>
+	
+	
+
+  </div>
+
+  </div>
